e4d7e5fd81e86b6ffdc95fda309bc76ed36b379c
Cecilia1998 <1622292369@qq.com>
2020-08-27 17:53:33 +0800

    Update globalconf.js

--- static/globalconf.js
+++ static/globalconf.js
@@ -1,6 +1,6 @@
 const globalconf = {
   axios: {
-    // baseURL: 'http://192.168.1.106:9527/wms/'
+    // baseURL: 'http://192.168.1.106:9527/wmstest/'
     // baseURL: 'http://192.168.1.135:8091/'
   }
 }
